d2549e48321dee381aa6cb21e22f1589cb1d104c
Jetracon <vovalucky80849@gmail.com>
2024-01-18 10:39:43 +0200

    Implement 'Send All!' feature

--- hillelChannels2/settings.py
+++ hillelChannels2/settings.py
@@ -17,7 +17,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 ALLOWED_HOSTS = []
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
